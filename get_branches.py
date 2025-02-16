96f1bdc96df0c130fa27d5d9d2d799c9816f9a97
CNOCTAVE <cnoctave@qq.com>
2025-02-16 13:21:18 +0800

    test github-actions
    test github-actions

--- get_branches.py
+++ get_branches.py
@@ -2,19 +2,24 @@ import subprocess
 import os
 import fnmatch
 import json
+import datetime
+
 
 def find_first_tar_file(directory):
     for root, dirs, files in os.walk(directory):
         for filename in files:
             if fnmatch.fnmatch(filename, '*.tar'):
-                return os.path.join(root, filename)
+                return filename
     return None
+
 
 def main():
     try:
-        output = subprocess.check_output(['git', 'branch', '-a'], stderr=subprocess.STDOUT, text=True)
+        output = subprocess.check_output(
+            ['git', 'branch', '-a'], stderr=subprocess.STDOUT, text=True)
         print(f"Git branches:\n{output}\n")
-        output_list = output.replace('-> origin/master', '').strip().split('\n')
+        output_list = output.replace(
+            '-> origin/master', '').strip().split('\n')
         print(f"Git branch list:\n{output_list}\n")
         branch_list = list()
         for line in output_list:
@@ -25,17 +30,25 @@ def main():
         docker_brew_fedora_list = list()
         for branch in branch_list:
             try:
-                output = subprocess.check_output(['git', 'checkout', branch], stderr=subprocess.STDOUT, text=True)
+                output = subprocess.check_output(
+                    ['git', 'checkout', branch], stderr=subprocess.STDOUT, text=True)
                 docker_brew_fedora = find_first_tar_file('./x86_64')
                 if docker_brew_fedora is not None:
-                    docker_brew_fedora_list.append({'branch': branch, 'docker_brew_fedora': docker_brew_fedora})
+                    docker_brew_fedora_list.append(
+                        {
+                            'branch': branch, 
+                            'date': datetime.datetime.date(datetime.datetime.now()).strftime("%Y%m%d"),
+                            'docker_brew_fedora': f'https://raw.githubusercontent.com/fedora-cloud/docker-brew-fedora/refs/heads/41/x86_64/{docker_brew_fedora}'
+                        })
             except subprocess.CalledProcessError as e:
                 print(f"Cannot get docker_brew_fedora, skip: {e.output}")
         print(f"Docker brew fedora list:\n{docker_brew_fedora_list}\n")
-        json.dump(docker_brew_fedora_list, open('../docker_brew_fedora_list.json', 'w'), indent=4)
-            
+        json.dump(docker_brew_fedora_list, open(
+            '../docker_brew_fedora_list.json', 'w'), indent=4)
+
     except subprocess.CalledProcessError as e:
         print(f"Cannot get git branch: {e.output}")
+
 
 if __name__ == '__main__':
     main()
